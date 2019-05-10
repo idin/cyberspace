8bbd07fce1fb31d2698525b435b936884d3c7226
Idin <d@idin.net>
2019-05-09 23:56:40 -0700

    fix find_unique

--- internet/beautiful_soup_helpers/get_lists.py
+++ internet/beautiful_soup_helpers/get_lists.py
@@ -1,9 +1,8 @@
 from bs4 import Tag
 from .clone_beautiful_soup_tag import clone_beautiful_soup_tag
-from .find_links import parse_link, find_links
+from .find_links import find_links
 
-
-def find_all(element, **kwargs):
+def find_unique(element, **kwargs):
 	"""
 	:type element: Tag
 	:rtype: list
@@ -12,32 +11,54 @@ def find_all(element, **kwargs):
 	result = []
 	first_child = element_copy.find(**kwargs)
 	while first_child:
-		result.append(clone_beautiful_soup_tag(first_child))
-		first_child.decompose()
+		result.append(first_child)
+		first_child.extract()
 		first_child = element_copy.find(**kwargs)
 	return result
 
 
-def get_items(list_element, links_only=False, base=None):
-	items = find_all(element=list_element, name='li')
+def _find_unique(element, **kwargs):
+	try:
+		found = element.find_all(recursive=False, **kwargs)
+	except AttributeError:
+		return []
+	if found:
+		if isinstance(found, list):
+			try:
+				children = element.children
+				return [find_unique(child, **kwargs) for child in children]
+			except AttributeError:
+				return found
+		else:
+			return []
+	else:
+		result = []
+		try:
+			children = element.children
+		except AttributeError:
+			return []
+		for child in children:
+			result += find_unique(child, **kwargs)
+		return result
+
+
+def get_items(element, links_only=False, base=None):
+	#items = element.find_all('li', recursive=False)
+	items = find_unique(element=element, name='li')
 	results = [get_lists(i, links_only=links_only, base=base) for i in items]
 	return [x for x in results if x is not None]
 
 
 def get_lists(element, links_only=False, base=None):
 	if element.find('ul'):
-		lists = find_all(element=element, name='ul')
+		#lists = element.find_all('ul')
+		lists = find_unique(element=element, name='ul')
 		return [get_items(l, links_only=links_only, base=base) for l in lists]
 	else:
 		if links_only:
-			links = find_links(element=element, base=base)
-			if len(links) == 0:
-				return None
-			elif len(links) == 1:
-				return links[0]
-			else:
-				return links
+			return find_links(element=element, base=base)
 		else:
 			return element
+
 
 
