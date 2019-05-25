e5d2f0743b4487e5550ad55c234fb878a2765221
Idin <d@idin.net>
2019-05-25 13:59:16 -0700

    add ravenclaw

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ setup(
 	packages=find_packages(exclude=["jupyter_tests", ".idea", ".git"]),
 	install_requires=[
 		'requests', 'bs4', 'selenium', 'chronology', 'fake_useragent', 'datefinder',
-		'slytherin', 'interaction', 'abstract', 'pensieve', 'linguistics', 'pandas'
+		'slytherin', 'interaction', 'abstract', 'pensieve', 'linguistics', 'pandas', 'ravenclaw'
 	],
 	python_requires='~=3.6',
 	zip_safe=False
